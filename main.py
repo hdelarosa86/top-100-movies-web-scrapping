60642846aaccf78bd834c8751f242a7947e01e79
Hector DelaRosa <hdelaros@gmail.com>
2023-04-07 00:35:51 -0400

    Fixed endpoint, cleaned up code, got rid of counter

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
-response = requests.get('https://www.empireonline.com/movies/features/best-movies-2/')
+response = requests.get('https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/')
 web_page_data = response.text
 
 soup = BeautifulSoup(web_page_data, 'html.parser')
 
-movie_titles_list = soup.select(selector='.listicle-item-content p:nth-child(2) a')
-text_list = [titles.getText()[24:] for titles in movie_titles_list]
+movie_titles_list = soup.select(selector='.article-title-description__text .title')
+text_list = [titles.getText() for titles in movie_titles_list]
 
-count = 1
 with open('top-100-movies.txt', 'w') as file:
     for items in reversed(text_list):
-        file.write(f'{count}) {items}\n')
-        count += 1
+        file.write(f'{items}\n')
