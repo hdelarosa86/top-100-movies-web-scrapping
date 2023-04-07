09763f0e00bd2ed212dacd28da0233d89c93f3c4
Hector DelaRosa <hdelaros@gmail.com>
2023-04-07 00:25:09 -0400

    Write list to txt file

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
+response = requests.get('https://www.empireonline.com/movies/features/best-movies-2/')
+web_page_data = response.text
 
+soup = BeautifulSoup(web_page_data, 'html.parser')
+
+movie_titles_list = soup.select(selector='.listicle-item-content p:nth-child(2) a')
+text_list = [titles.getText()[24:] for titles in movie_titles_list]
+
+with open('top-100-movies.txt', 'w') as file:
+    for items in reversed(text_list):
+        file.write(f'{items}\n')
